339ff2a81fe5d2d057786c7b6a478aa786593316
manasapala <33602592+manasapala@users.noreply.github.com>
2020-11-19 20:36:07 -0800

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_azs" {
+  description = "comma separated string of availability zones in order of precedence"
+  default     = "us-east-2a, us-east-2b, us-east-2c"
+}
+variable "az_count" {
+  description = "number of active availability zones in VPC"
+  default     = "3"
+}
+variable "cluster_name" {
+  default = "terraform-eks-vgd"
+  type    = string
+}
