ee69e0ee3af9366d027293083c1483bd08a14797
manasapala <33602592+manasapala@users.noreply.github.com>
2020-11-23 19:16:11 -0800

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 variable "aws_azs" {
   description = "comma separated string of availability zones in order of precedence"
